993b8e2bcc4de2d864cf58871d896a75966ee362
Graeme Gregory <graeme@xora.org.uk>
2023-11-28 02:31:16 +0000

    build-mingw.sh : updates
    Signed-off-by: Graeme Gregory <graeme@xora.org.uk>

--- build-mingw.sh
+++ build-mingw.sh
@@ -1,9 +1,9 @@
 #! /bin/sh
 #
 
-sed -i "s|ac_exeext=|ac_exeext=.exe|" libiberty/configure
-sed -i "s|ac_exeext=|ac_exeext=.exe|" gcc/configure
+sed -i "s|ac_exeext=$|ac_exeext=.exe|" libiberty/configure
+sed -i "s|ac_exeext=$|ac_exeext=.exe|" gcc/configure
 sed -i "s|USE_COLLECT2 = .*|USE_COLLECT2 =|" gcc/Makefile.in
 
-FLAGS=-Wno-implicit-int ./configure --host=i686-mingw32 --target=qdos --disable-use_collect2
+CFLAGS=-Wno-implicit-int CPP=i686-w64-mingw32-cpp CC=i686-w64-mingw32-gcc ./configure --prefix=/qdos/ --host=i686-mingw32 --target=qdos
 
