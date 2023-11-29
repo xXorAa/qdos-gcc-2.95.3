22ef15263c7ce7ddcccdf807e752440e1eab4cb0
Graeme Gregory <graeme@xora.org.uk>
2023-11-29 20:36:05 +0000

    configure-mingw.sh : more fixes/improvements
    Signed-off-by: Graeme Gregory <graeme@xora.org.uk>

--- configure-mingw.sh
+++ configure-mingw.sh
@@ -8,5 +8,9 @@ sed -i "s|ac_exeext=$|ac_exeext=.exe|" gcc/configure
 sed -i "s|USE_COLLECT2 = .*|USE_COLLECT2 =|" gcc/Makefile.in
 patch -p1 < qdos/mingw/0001-gcc-config-m68k-qdos.h-alter-paths-for-mingw-port.patch
 
-CFLAGS=-Wno-implicit-int CPP=i686-w64-mingw32-cpp CC=i686-w64-mingw32-gcc ./configure --prefix=/qdos-gcc/ --host=i686-mingw32 --target=qdos --exec-prefix=/
+pushd qdos/mingw
+make
+popd
+
+CFLAGS="-Wno-implicit-int" ./configure --prefix=/qdos-gcc/ --host=i686-mingw32 --target=qdos --exec-prefix=/
 
