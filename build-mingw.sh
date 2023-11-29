06284e774baaaa3f0ff58ac113c69cd834596340
Graeme Gregory <graeme@xora.org.uk>
2023-11-29 18:56:14 +0000

    build-mingw.sh : updates to build script
    Signed-off-by: Graeme Gregory <graeme@xora.org.uk>

--- build-mingw.sh
+++ build-mingw.sh
@@ -1,9 +1,12 @@
 #! /bin/sh
 #
 
+git checkout .
+
 sed -i "s|ac_exeext=$|ac_exeext=.exe|" libiberty/configure
 sed -i "s|ac_exeext=$|ac_exeext=.exe|" gcc/configure
 sed -i "s|USE_COLLECT2 = .*|USE_COLLECT2 =|" gcc/Makefile.in
+patch -p1 < qdos/mingw/0001-gcc-config-m68k-qdos.h-alter-paths-for-mingw-port.patch
 
-CFLAGS=-Wno-implicit-int CPP=i686-w64-mingw32-cpp CC=i686-w64-mingw32-gcc ./configure --prefix=/qdos/ --host=i686-mingw32 --target=qdos
+CFLAGS=-Wno-implicit-int CPP=i686-w64-mingw32-cpp CC=i686-w64-mingw32-gcc ./configure --prefix=/qdos-gcc/ --host=i686-mingw32 --target=qdos --exec-prefix=/
 
