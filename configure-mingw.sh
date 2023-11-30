46d3ad2052596eb0cd244ea0fbdf55abc61bb630
Graeme Gregory <graeme@xora.org.uk>
2023-11-30 09:58:19 +0000

    configure-mingw.sh : change path internal exes are searched on
    Signed-off-by: Graeme Gregory <graeme@xora.org.uk>

--- configure-mingw.sh
+++ configure-mingw.sh
@@ -6,6 +6,7 @@ git checkout .
 sed -i "s|ac_exeext=$|ac_exeext=.exe|" libiberty/configure
 sed -i "s|ac_exeext=$|ac_exeext=.exe|" gcc/configure
 sed -i "s|USE_COLLECT2 = .*|USE_COLLECT2 =|" gcc/Makefile.in
+sed -i "s|/usr/lib/gcc/|/qdos-gcc/bin/|" gcc/gcc.c
 patch -p1 < qdos/mingw/0001-gcc-config-m68k-qdos.h-alter-paths-for-mingw-port.patch
 
 pushd qdos/mingw
